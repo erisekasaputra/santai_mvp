b142945f78cf97153173598910acc8e0fb5a113a
Eris Eka Saputra <erisekasaputra282000@gmail.com>
2024-09-25 19:09:14 +0700

    updated docker file

--- Master.Data.API/obj/Debug/net9.0/Master.Data.API.AssemblyInfo.cs
+++ Master.Data.API/obj/Debug/net9.0/Master.Data.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Master.Data.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d314dda45eaceb818e635cae335268967f18cb99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ca49cb7717b6e9b25e494ce4d242387b426607d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Master.Data.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Master.Data.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
