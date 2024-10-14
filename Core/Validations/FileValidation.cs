3b294644e9d5e1b5b6aa82250a261a75d6748848
Eris Eka Saputra <erisekasaputra282000@gmail.com>
2024-10-15 06:53:22 +0700

    updated:

--- Core/Validations/FileValidation.cs
+++ Core/Validations/FileValidation.cs
@@ -1,22 +1,23 @@
 ﻿using Microsoft.AspNetCore.Http;
+using System.Text.RegularExpressions;
 
 namespace Core.Validations; 
 public class FileValidation
 {
     private static readonly string[] PermittedExtensions =
-    {
+    [
         ".jpg", ".jpeg", ".png", ".gif", ".webp", ".svg"
-    };
+    ];
 
     private static readonly string[] PermittedMimeTypes =
-    {
+    [
         "image/jpeg",    // .jpeg
         "image/png",     // .png
         "image/gif",     // .gif
         "image/webp",    // .webp
         "image/svg+xml",  // .svg
         "image/jpg"
-    };
+    ];
 
 
     public static bool IsValidImage(IFormFile file)
@@ -34,5 +35,12 @@ public class FileValidation
             return false;
 
         return true;
-    }
+    }  
+
+    public static bool IsValidImageFileName(string fileName)
+    { 
+        string pattern = @"^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}\.(jpg|jpeg|png|gif|webp|svg)$";
+
+        return Regex.IsMatch(fileName, pattern);
+    } 
 }
