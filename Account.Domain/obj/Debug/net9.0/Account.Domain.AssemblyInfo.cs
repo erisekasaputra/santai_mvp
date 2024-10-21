d4179bc65129a58bef592da63ef4375b62c5228a
Eris Eka Saputra <erisekasaputra282000@gmail.com>
2024-10-21 12:09:21 +0700

    edited connection string

--- Account.Domain/obj/Debug/net9.0/Account.Domain.AssemblyInfo.cs
+++ Account.Domain/obj/Debug/net9.0/Account.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Account.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0b8eae97093f2b32a4399336c613ef2c685af84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5af24de0801c22b9464bdf6dabe7c3f5685541fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Account.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Account.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0b8eae97093f2b32a4399336c613ef2c685af84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5af24de0801c22b9464bdf6dabe7c3f5685541fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
