67292702c2d60f80b67f3e1754ffd58b08ee898c
Eris Eka Saputra <erisekasaputra282000@gmail.com>
2024-11-07 14:33:58 +0700

    no comment

--- Chat.API/Applications/Dtos/Request/LatestChatRequest.cs
+++ Chat.API/Applications/Dtos/Request/LatestChatRequest.cs
@@ -2,8 +2,7 @@
 
 public class LatestChatRequest
 {
-    public Guid OrderId { get; set; }
-    public long Timestamp { get; set; }
+    public Guid OrderId { get; set; } 
     public bool Forward { get; set; }
 
     public LatestChatRequest()
@@ -11,12 +10,10 @@ public class LatestChatRequest
         
     }
     public LatestChatRequest(
-        Guid orderId,
-        long timestamp,
+        Guid orderId, 
         bool forward)
     {
-        OrderId = orderId;
-        Timestamp = timestamp;
+        OrderId = orderId; 
         Forward = forward;
     }
 }
