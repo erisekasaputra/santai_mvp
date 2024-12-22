775ac0e19f36b41dc154c3b10bf025f29ad07769
Eris Eka Saputra <erisekasaputra282000@gmail.com>
2024-12-22 16:58:42 +0700

    validation plate number for account service

--- Account.API/Validations/FleetValidations/UpdateFleetValidation.cs
+++ Account.API/Validations/FleetValidations/UpdateFleetValidation.cs
@@ -21,67 +21,9 @@ public class UpdateFleetValidation : AbstractValidator<UpdateFleetRequestDto>
             .NotEmpty()
             .WithMessage("Image resource name can not be empty");
 
-        RuleFor(x => x.RegistrationNumber) 
+        RuleFor(x => x.RegistrationNumber)
             .NotEmpty().WithMessage("Plate number cannot be empty.")
-            .Matches("^[A-Z]{1,3}\\s*[0-9]{1,4}\\s*[A-Z]?$")
-            .WithMessage("Plate number should be in the format: 'W 1234 A', 'W1234A', or 'ABC 1234'.")
-            .Matches("^Q[A-Z]{1,2}\\s*[0-9]{1,4}\\s*[A-Z]?$")
-            .WithMessage("Sarawak plate should be in the format: 'QA 1234 A', or 'QAB 1234'.")
-            .Matches("^KV\\s*[0-9]{1,4}\\s*[A-Z]?$")
-            .WithMessage("Langkawi plate should be in the format: 'KV 1234 A'.")
-            .Matches("^H[A-Z]{1,2}\\s*[0-9]{1,4}$")
-            .WithMessage("Taxi plate should be in the format: 'HBA 1234' or 'HWD 1234'.")
-            .Matches("^([0-9]{1,4}\\s*CD|[0-9]{1,2}\\s*CD\\s*[0-9]{1,2})$")
-            .WithMessage("Diplomatic plate should be in the format: '1234 CD', '12 CD 34'.")
-            .Matches("^Z[A-Z]?\\s*[0-9]{1,4}$")
-            .WithMessage("Military plate should be in the format: 'Z 1234' or 'ZD 1234'.")
-            .Matches("^(DYMM|Raja|SUK)\\s*[0-9]{1,4}$")
-            .WithMessage("Royal plate should be in the format: 'DYMM 1', 'SUK 1234'.")
-            .Matches("^[A-Z]\\s*[0-9]{1,4}\\s*[A-Z]$")
-            .WithMessage("Trade plate should be in the format: 'P 1234 A', 'S 1234 A'.");
-    }
-
-    private bool BeAValidMalaysianPlate(string? plateNumber)
-    {
-        if (string.IsNullOrWhiteSpace(plateNumber)) return false;
-
-        // Define regex patterns for various plate types
-        var patterns = new[]
-        {
-            // Standard plate (e.g., W1234A, ABC1234, W123A)
-            "^[A-Z]{1,3}[0-9]{1,4}[A-Z]?$",
-
-            // Sarawak plate (e.g., QA1234A, QAB1234)
-            "^Q[A-Z]{1,2}[0-9]{1,4}[A-Z]?$",
-
-            // Langkawi plate (e.g., KV1234A)
-            "^KV[0-9]{1,4}[A-Z]?$",
-
-            // Taxi plate (e.g., HBA1234, HWD1234)
-            "^H[A-Z]{1,2}[0-9]{1,4}$",
-
-            // Diplomatic plate (e.g., 1234CD, 12CD34)
-            "^([0-9]{1,4}CD|[0-9]{1,2}CD[0-9]{1,2})$",
-
-            // Military plate (e.g., Z1234, ZD1234)
-            "^Z[A-Z]?[0-9]{1,4}$",
-
-            // Royal plate (e.g., DYMM1, SUK1234)
-            "^(DYMM|Raja|SUK)[0-9]{1,4}$",
-
-            // Trade plates (e.g., P1234A, S1234A)
-            "^[A-Z][0-9]{1,4}[A-Z]$",
-        };
-
-        // Check if the plate number matches any pattern
-        foreach (var pattern in patterns)
-        {
-            if (Regex.IsMatch(plateNumber, pattern, RegexOptions.IgnoreCase))
-            {
-                return true;
-            }
-        }
-
-        return false;
-    }
+            .Matches(@"(^[A-Z]{1,3}\s*[0-9]{1,4}\s*[A-Z]?$)|(^Q[A-Z]{1,2}\s*[0-9]{1,4}\s*[A-Z]?$)|(^KV\s*[0-9]{1,4}\s*[A-Z]?$)|(^H[A-Z]{1,2}\s*[0-9]{1,4}$)|(^[0-9]{1,4}\s*CD|[0-9]{1,2}\s*CD\s*[0-9]{1,2}$)|(^Z[A-Z]?\s*[0-9]{1,4}$)|(^DYMM|Raja|SUK\s*[0-9]{1,4}$)|(^[A-Z]\s*[0-9]{1,4}\s*[A-Z]$)")
+            .WithMessage("Plate number should match one of the following formats: 'W 1234 A', 'W1234A', 'ABC 1234', 'QA 1234 A', 'QAB 1234', 'KV 1234 A', 'HBA 1234', '12 CD 34', 'Z 1234', 'DYMM 1234', 'P 1234 A'.");
+    } 
 }
--- Catalog.Infrastructure/Repositories/ItemRepository.cs
+++ Catalog.Infrastructure/Repositories/ItemRepository.cs
@@ -43,6 +43,7 @@ public class ItemRepository(CatalogDbContext context, MetaTableHelper metaTableH
         var query = _context.Items
             .Where(w => !w.IsDeleted)
             .AsQueryable(); 
+
         if (categoryId is not null && categoryId.HasValue && categoryId != Guid.Empty) 
         {
             query = query.Where(x => x.CategoryId == categoryId);
